27c7be08d8e8c4c8d0f31a1c4f19890555be2a33
Nikita <zininnikita309@gmail.com>
2022-06-16 23:18:45 +0900

    rename

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,21 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
-    public UnityEngine.AI.NavMeshAgent enemy;
-    public Transform Player;
+    public NavMeshAgent agent;
 
-    void Start()
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("PlaerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patrolling()
+    {
+
+    }
+
+    private void ChasePlayer()
     {
         
     }
 
-    void Update()
+    private void AttackPlayer()
     {
-        enemy.SetDestination(Player.position);
+
     }
 }
 
