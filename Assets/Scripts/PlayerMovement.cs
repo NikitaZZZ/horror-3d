c5d8666a26a87e65409f9f9530fcc0e04b24d2f5
Nikita <zininnikita309@gmail.com>
2022-06-19 00:11:39 +0900

    made a walking crouch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    // private bool ShouldCroch => Input.GetKeyDown(crouchKey) && isGrounded;
 
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    [SerializeField] public CharacterController controller;
 
-    Vector3 velocity;
-    bool isGrounded;
+    [SerializeField] public float speed = 12f;
+    [SerializeField] public float gravity = -9.81f;
+    [SerializeField] public float jumpHeight = 3f;
 
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundMask;
-    public float groundDistance = 0.4f;
+    [SerializeField] public Transform groundCheck;
+    [SerializeField] public LayerMask groundMask;
+    [SerializeField] public float groundDistance = 0.4f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private bool isCrouching;
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
-            velocity.y = -2f;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            HandleCrouch();
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -36,4 +40,39 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
+
+    private void HandleCrouch() 
+    { 
+        isCrouching = !isCrouching;
+
+        if (isCrouching)
+            controller.height = 0.5f;
+        else
+            controller.height = 1.6f;
+    }
+
+    // private IEnumerator CrochStand()
+    // {
+    //     if (isCrouching && Physics.Raycast(GameObject.Find("Main Camera").transform.position, Vector3.up, 1f))
+    //         yield break;
+
+    //     float timeElapsed = 0;
+    //     float targetHeight = isCrouching ? standingHeight : crouchHeight;
+    //     float currentHeight = controller.height;
+    //     Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+    //     Vector3 currentCenter = controller.center;
+
+    //     while (timeElapsed < timeToCrouch)
+    //     {
+    //         controller.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed/timeToCrouch);
+    //         controller.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed/timeToCrouch);
+    //         timeElapsed += Time.deltaTime;
+    //         yield return null;
+    //     }
+
+    //     controller.height = targetHeight;
+    //     controller.center = targetCenter;
+
+    //     isCrouching = !isCrouching;
+    // }
 }
