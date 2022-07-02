f297cb6c5894a6b9eeeef4ce8c71ae4efa968d75
Nikita <zininnikita309@gmail.com>
2022-07-02 21:46:12 +0900

    inventory && take item && quest system

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Quest : MonoBehaviour
+{
+    [SerializeField] string[] tasks = new string[5] { "Прыгнуть", "2", "3", "4", "5" };
+    [SerializeField] private TMP_Text currentTask = default;
+    [SerializeField] public GameObject[] objectForEndTask;
+
+    private int current_task = 0; 
+
+    void OnTriggerEnter(Collider obj) {
+        if (obj.GetComponent<Collider>().name == objectForEndTask[current_task].name)
+        {
+            current_task += 1;
+            UpdateTask();
+        }
+    }
+
+    private void UpdateTask()
+    {
+        currentTask.SetText(tasks[current_task]);
+    }
+}
