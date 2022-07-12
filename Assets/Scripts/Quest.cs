7d49095a1c6233f3a2a15761e39fea0eccb58029
Nikita <zininnikita309@gmail.com>
2022-07-12 22:46:28 +0900

    quest system is ready on scene 'flex'

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -6,18 +6,23 @@ using System;
 
 public class Quest : MonoBehaviour
 {
-    [SerializeField] string[] tasks = new string[5] { "Прыгнуть", "2", "3", "4", "5" };
+    [SerializeField] string[] tasks = new string[5] { "Поднять ключи", "2", "3", "4", "5" };
     [SerializeField] private TMP_Text currentTask = default;
     [SerializeField] public GameObject[] objectForEndTask;
 
     private int current_task = 0; 
 
+    void Awake() 
+    {
+        UpdateTask();
+    }
+
     void OnTriggerEnter(Collider obj) {
-        // if (obj.GetComponent<Collider>().name == objectForEndTask[current_task].name)
-        // {
-        //     current_task += 1;
-        //     UpdateTask();
-        // }
+        if (obj.GetComponent<Collider>().name == objectForEndTask[current_task].name)
+        {
+            current_task += 1;
+            UpdateTask();
+        }
     }
 
     private void UpdateTask()
